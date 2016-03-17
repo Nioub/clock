97fa72db86533062e8b5ff7d646b8a73a037ba3d
Thomas Auffredou <tauffredou@xebia.fr>
2016-03-17 22:11:54 +0100

    go fmt

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-  "time"
-  log "github.com/Sirupsen/logrus" // Use some external dep
+	log "github.com/Sirupsen/logrus" // Use some external dep
+	"time"
 )
 
 func main() {
-    log.Println(time.Now())
+	log.Println(time.Now())
 }
