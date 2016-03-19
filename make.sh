e3a38361424aba5925a08b1f2b28f610eb389000
Thomas Auffredou <tauffredou@xebia.fr>
2016-03-19 23:26:25 +0100

    Beautify make script

--- make.sh
+++ make.sh
@@ -1,14 +1,43 @@
 #!/bin/sh
 
 CWD=$(cd $(dirname $0);pwd)
+MAKE=$0
 IMAGE=tauffredou/clock
-docker build -f build.Dockerfile -t $IMAGE-builder .
 
-docker run -v $CWD:/go/src/github.com/tauffredou/clock -v $CWD/bin:/output $IMAGE-builder
+usage(){
+cat<<-EOUSAGE
+make.sh [Action]
+Actions:
+  builder       create builder image
+  build         create binary using builder image
+  image         create final image
+  run           run the final image
+  build-chain           builder,build,image
 
-docker build -t $IMAGE .
+EOUSAGE
+}
 
-echo
-echo run clock with
-echo docker run $IMAGE
-echo
+case $1 in
+  builder)
+    docker build -f build.Dockerfile -t $IMAGE-builder .
+  ;;
+  build)
+    docker run -v $CWD:/go/src/github.com/tauffredou/clock -v $CWD/bin:/output $IMAGE-builder
+    echo "Built in $CWD/bin"
+  ;;
+  image)
+    docker build -t $IMAGE .
+    echo
+    echo run clock with: docker run $IMAGE
+    echo
+  ;;
+  run)
+    docker run $IMAGE
+  ;;
+  all)
+    $MAKE builder && $MAKE build && $MAKE image
+  ;;
+  *)
+    usage
+  ;;
+esac
